4f2464bcd5ae336f99d22964a7072d804b8b3c99
NikitaPr0705 <iamnik0705@gmail.com>
2020-02-28 17:28:09 +0200

    Add changes to tests and task of loops

--- src/HW1/loops/task1.js
+++ src/HW1/loops/task1.js
@@ -1,7 +1,7 @@
-let sumOfEvenNum = 0;
-let countOfEven = 0;
 
 function countSumEvenNum() {
+  let sumOfEvenNum = 0;
+  let countOfEven = 0;
   for (let i = 1; i <= 99; i++) {
     if (i % 2 === 0) {
       sumOfEvenNum += i;
@@ -11,4 +11,4 @@ function countSumEvenNum() {
   return `Summ: ${sumOfEvenNum}, quantity: ${countOfEven}`;
 };
 
-countSumEvenNum();
+console.log(countSumEvenNum());

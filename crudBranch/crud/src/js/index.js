a38caaae78cd4e63620234a18dd7f973a934ea37
NikitaPr0705 <iamnik0705@gmail.com>
2020-03-11 15:51:09 +0200

    added crud code

--- crudBranch/crud/src/js/index.js
+++ crudBranch/crud/src/js/index.js
@@ -1,59 +1,65 @@
-const userId = document.getElementById('id');
-const firstName = document.getElementById('first-name');
-const lastName = document.getElementById('last-name');
-const age = document.getElementById('age');
-
-const createButton = document.querySelectorAll('create');
-const readButton = document.querySelectorAll('read');
-const updateButton = document.querySelectorAll('update');
-const deleteButton = document.querySelectorAll('delete');
 
 
-const idCell = document.getElementById("id-cell")
-const firstNameCell = document.getElementById("first-name-cell");
-const lastNameCell = document.getElementById("last-name-cell");
-const ageCell = document.getElementById("age-cell");
+const lsRadio = document.querySelector('.localstorage-radio');
+const indexDbRadio = document.querySelector('.indexedDB-radio');
 
 
-let userDataArray = [];
+const userId = document.querySelector('');
+// const firstName = document.getElementById('first-name');
+// const lastName = document.getElementById('last-name');
+// const age = document.getElementById('age');
 
-createButton.addEventListener('click', addDataToTable) {
-    console.log('createButtonClicked');
-}
-
-const createObj = {
-    userIdKey: +userId.value,
-    firstNameKey: firstName.value,
-    lastNameKey: lastName.value,
-    ageKey: +age.value,
-};
-console.log(createObj);
+// const createButton = document.querySelectorAll('create');
+// const readButton = document.querySelectorAll('read');
+// const updateButton = document.querySelectorAll('update');
+// const deleteButton = document.querySelectorAll('delete');
 
 
+// const idCell = document.getElementById("id-cell")
+// const firstNameCell = document.getElementById("first-name-cell");
+// const lastNameCell = document.getElementById("last-name-cell");
+// const ageCell = document.getElementById("age-cell");
 
-function addDataToTable(event) {
 
-    let idCell = document.querySelector(".col");
-    idCell.innerHTML = createObj.userIdKey;
-    let idCol = document.getElementsByClassName(".col1");
-    // createObj.userIdKey.append(idCell);
-    idCol.append(idCell);
-    // idCell += createObj.userIdKey;
-    // console.log("lalalal")
-}
+// let userDataArray = [];
+
+// createButton.addEventListener('click', addDataToTable) {
+//     console.log('createButtonClicked');
+// }
+
+// const createObj = {
+//     userIdKey: +userId.value,
+//     firstNameKey: firstName.value,
+//     lastNameKey: lastName.value,
+//     ageKey: +age.value,
+// };
+// console.log(createObj);
+
+
+
+// function addDataToTable(event) {
+
+//     let idCell = document.querySelector(".col");
+//     idCell.innerHTML = createObj.userIdKey;
+//     let idCol = document.getElementsByClassName(".col1");
+//     // createObj.userIdKey.append(idCell);
+//     idCol.append(idCell);
+//     // idCell += createObj.userIdKey;
+//     // console.log("lalalal")
+// }
 
 
 // function updateDataToTable() {
 //     idCell.innerHTML = createObj.userIdKey; // перезаписывает значение
 // }
 
-updateButton.onclick = function () {
-    console.log('update');
-}
+// updateButton.onclick = function () {
+//     console.log('update');
+// }
 
-deleteButton.onclick = function () {
-    console.log('delete');
-}
+// deleteButton.onclick = function () {
+//     console.log('delete');
+// }
 
 
 //////////////
@@ -69,3 +75,4 @@ deleteButton.onclick = function () {
 
 // localStorage - объект в DOM 
 //
+

d6878141fc32fca7f3d4c1690fb51c456b983e90
NikitaPr0705 <iamnik0705@gmail.com>
2020-03-19 14:06:01 +0200

    added changes

--- crudBranch/crud/src/js/index.js
+++ crudBranch/crud/src/js/index.js
@@ -5,13 +5,6 @@ const indexDbRadio = document.querySelector('.indexedDB-radio');
 
 const allInputs = document.querySelectorAll('.crud__input');
 
-
-// const userId = document.querySelector('.crud__id');
-// const firstName = document.querySelector('.crud__first-name');
-// const lastName = document.querySelector('.crud__last-name');
-// const age = document.querySelector('.crud__age');
-// const email = document.querySelector('.crud__email');
-// const phone = document.querySelector('.crud__phone');
 const list = document.querySelector('.table__nav');
 
 const createButton = document.querySelector('.buttons__create-button');
@@ -20,19 +13,26 @@ const deleteButton = document.querySelector('.buttons__delete-button');
 
 
 
-const userDataArray = [];
+const userDataArray = []; // массив объектов (юзеров в таблице)
 
-function clearFields() {
+function clearFields() { // очистка полей после ввода
     for(let i = 0; i < allInputs.length; i++) {
         allInputs[i].value = '';
+    }
+}
+
+function putInDock(line, objArray) {
+    debugger
+    for(let key in objArray) {
+        line.innerHTML += `<div class="row-text row-text--normal-font ${key}-cell">${objArray[key]}</div>`
     }
 }
 
 function pushDataToTable(arr) {
     const userData = function() {
         this.id = arr[0].value,
-        this.firstName = arr[1].value,
-        this.lastName = arr[2].value,
+        this.firstname = arr[1].value,
+        this.lastname = arr[2].value,
         this.age = arr[3].value,
         this.email = arr[4].value,
         this.phone = arr[5].value
@@ -43,7 +43,7 @@ function pushDataToTable(arr) {
 };
 
 createButton.addEventListener('click', () => {
-        if (allInputs[0].value == '' || allInputs[1].value == '' || allInputs[2].value == '' || allInputs[3].value == '' || allInputs[4].value == '' || allInputs[5].value == '') {
+        if(allInputs[0].value == '' || allInputs[1].value == '' || allInputs[2].value == '' || allInputs[3].value == '' || allInputs[4].value == '' || allInputs[5].value == '') {
         clearFields();
         alert('Some fields are empty');
         return
@@ -53,103 +53,40 @@ createButton.addEventListener('click', () => {
         console.log(person);
         const rowLine = document.createElement('li');
         rowLine.className = 'table__list';
-        rowLine.innerHTML += `<div class="first-row-text first-row-text--normal-font id-cell">${person.id}</div>
-        <div class="first-row-text first-row-text--normal-font first-name-cell">${person.firstName}</div>
-        <div class="first-row-text first-row-text--normal-font last-name-cell">${person.lastName}</div>
-        <div class="first-row-text first-row-text--normal-font age-cell">${person.age}</div>
-        <div class="first-row-text first-row-text--normal-font email-cell">${person.email}</div>
-        <div class="first-row-text first-row-text--normal-font phone-cell">${person.phone}</div>`;
+        putInDock(rowLine, person);
         list.append(rowLine);
         clearFields();
         console.log(userDataArray);
 });
 
 
-
-
 updateButton.addEventListener('click', () => {
+    debugger
     const user = pushDataToTable(allInputs);
     const rows = document.querySelectorAll('.table__list');
 
     for(let i = 0; i < userDataArray.length; i++) {
         if(userDataArray[i].id == user.id) {
-            if(user.firstName !== '') {
-                userDataArray[i].firstName = user.firstName;
+            for(let key in userDataArray[i]) {
+                if(user[key] !== '') {
+                    userDataArray[i][key] = user[key];
+                }
             }
-            if(user.lastName !== '') {
-                userDataArray[i].lastName = user.lastName;
-            }
-            if(user.age !== '') {
-                userDataArray[i].age = user.age;
-            }
-            if(user.email !== '') {
-                userDataArray[i].email = user.email;
-            }
-            if(user.phone !== '') {
-                userDataArray[i].phone = user.phone;
-            }
-            rows[i + 1].innerHTML = '';
-            rows[i + 1].innerHTML += `<div class="first-row-text first-row-text--normal-font id-cell">${userDataArray[i].id}</div>
-            <div class="first-row-text first-row-text--normal-font first-name-cell">${userDataArray[i].firstName}</div>
-            <div class="first-row-text first-row-text--normal-font last-name-cell">${userDataArray[i].lastName}</div>
-            <div class="first-row-text first-row-text--normal-font age-cell">${userDataArray[i].age}</div>
-            <div class="first-row-text first-row-text--normal-font email-cell">${userDataArray[i].email}</div>
-            <div class="first-row-text first-row-text--normal-font phone-cell">${userDataArray[i].phone}</div>`
+                    rows[i + 1].innerHTML = '';
+                    putInDock(rows[i + 1], userDataArray[i]);
         }
-        console.log(userDataArray[i]);
+    console.log(userDataArray);
     }
-
-})
-
+});
 
 
-
-
-
-// createButton.addEventListener('click', addDataToTable);
-
-// console.log(createObj);
-
-
-
-// function addDataToTable(event) {
-
-//     let idCell = document.querySelector(".id-cell");
-//     idCell.innerHTML = createObj.userIdKey;
-//     let idCol = document.getElementsByClassName(".id-cell");
-//     // createObj.userIdKey.append(idCell);
-//     idCol.append(idCell);
-//     console.log('createButtonClicked');
-
-    // idCell += createObj.userIdKey;
-    // console.log("lalalal")
-// };
-
-
-// function updateDataToTable() {
-//     idCell.innerHTML = createObj.userIdKey; // перезаписывает значение
-// }
-
-// updateButton.onclick = function () {
-//     console.log('update');
-// }
-
-// deleteButton.onclick = function () {
-//     console.log('delete');
-// }
-
-
-//////////////
-
-// в тестах объявить AList в it, и он становится общим для всех
-
-
-
-// localStorage
-
-
-
-
-// localStorage - объект в DOM 
-//
-
+deleteButton.addEventListener('click', () => {
+    const rows = document.querySelectorAll('.table__list');
+    for(let i = 0; i < userDataArray.length; i++) {
+        if(userDataArray[i].id == allInputs[0].value) {
+            userDataArray.splice(i, 1);
+            rows[i + 1].remove();
+            console.log(userDataArray);
+        }
+    }
+});
